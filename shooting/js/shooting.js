f5d89a9afec659d49ff5165f2faa01c0dbda804d
yama-t <y.sasajima@leihauoli.com>
2017-04-29 19:52:55 +0900

    岩と飛行機をvueコンポーネント化

--- shooting/js/shooting.js
+++ shooting/js/shooting.js
@@ -1,92 +1,92 @@
-
-new Vue({
-  el: '#app',
-  data: {
-    speed: 10,
-    barrett: '<div class="barrett" />',
-    planePos: '',
-    barrettAdjust: {
-      top: 50,
-      left: 20
+Vue.component('shooting-plane', {
+  template: '<div id="plane" class="plane" :style="{ left: planePos }"></div>',
+  mounted: function(){
+    this.$parent = this.$el.parentNode;
+    this.$barrett = document.createElement('div');
+    this.$barrett.classList.add('barrett');
+    this.planeWidth = this.$el.clientWidth;
+    this.shootEvent = new Event('shoot', {"bubbles": true, "cancelable": false});
+    this.bindEvent();
+    this.setBarrettRemove();
+  },
+  props: {
+    planePos: {
+      type: String,
+      default: ''
+    },
+    speed: {
+      type: Number,
+      default: 20
+    },
+    barretAdjust: {
+      type: Object,
+      default: {
+        top: 50,
+        left: 20
+      }
     }
   },
-  mounted: function(){
-    this.setParameters();
-    this.bindEvent();
-  },
   methods: {
-    setParameters: function(){
-      this.$plane = $('#plane');
-      this.$target = $('#target');
-      this.planeWidth = this.$plane.width();
-      this.targetWidth = this.$target.width();
-      this.targetAdjustWidth = this.targetWidth / 2;
-      this.targetDefaultPos = `calc(50% - ${this.targetAdjustWidth}px)`
-    },
     bindEvent: function(){
-      this.$target.on('transitionend webkitTransitionEnd', () => {
-        this.resetTargetPos();
-      });
+        window.addEventListener('keydown', (e) => {
+          this.controller(e.which);
+        }, false);
     },
-    resetTargetPos: function(){
-      this.$target.css('left', this.targetDefaultPos);
-    },
-    controller: function(e){
-      const key = e.which;
+    setBarrettRemove: function($barrett){
+      this.$parent.addEventListener('webkitAnimationEnd', (e) => {
+        this.removeBarrett(e);
+      }, true);
 
+      this.$parent.addEventListener('animationend', (e) => {
+        this.removeBarrett(e);
+      }, true);
+    },
+    controller: function(key){
       // 13: enter, 32: space, 90: z
       if([13, 32, 90].includes(key)) {
         this.shoot();
-        this.avoidTarget();
       }
 
       // 37: left, 39: right, 72: h, 76: l
       if([37, 39, 72, 76].includes(key)) {
-        this.slide(key);
+        this.move(key);
       }
     },
-    avoidTarget: function() {
-      const random = this.getRandom(0, 2);
+    move: function(key){
+      const pos = this.$el.getBoundingClientRect().left;
+      let movePos;
 
-      switch (random) {
-        case 0:
-          this.slideTarget('-100vw');
-          break;
-        case 1:
-          this.slideTarget('100vw');
-          break;
-        case 2:
-          clearTimeout(this.timer);
-          this.$target.css('opacity', '0');
-          this.timer = setTimeout(() => {
-              this.$target.css('opacity', '1');
-          }, 300);
-          break;
-       }
-    },
-    slideTarget: function(val){
-      this.$target.css('left', val);
+      // 37: left, 39: right
+      if([37, 72].includes(key)) {
+        movePos = pos - this.speed;
+      } else if([39, 76].includes(key)) {
+        movePos = pos + this.speed;
+      }
+
+      this.planePos = movePos + 'px';
     },
     shoot: function(){
-      const $plane = $('#plane'),
-            $app = $('#app'),
-          $b = $(this.barrett),
-          {top, left} = $plane.position(),
-          barretTop = top - 50,
-          barrettLeft = left + this.planeWidth - 20,
-          [R, G, B] = this.createRandomColor();
+      const $barrett = this.$barrett.cloneNode(),
+          {top, left} = this.$el.getBoundingClientRect(),
+          barrettTop = top - this.barretAdjust.top,
+          barrettLeft = left + this.planeWidth - this.barretAdjust.left,
+          [R, G, B] = this.createRandomColor(),
+          bs = $barrett.style;
 
-        $b.css({
-          top: barretTop,
-          left: barrettLeft,
-          backgroundColor: `rgb(${R}, ${G}, ${B})`
-        });
+          bs.top = barrettTop + 'px';
+          bs.left = barrettLeft + 'px';
+          bs.backgroundColor = `rgb(${R}, ${G}, ${B})`;
 
-      $b.on('animationend webkitAnimationEnd', function(){
-          $b.remove();
-      });
+          this.$el.parentNode.appendChild($barrett);
+          this.$el.dispatchEvent(this.shootEvent);
+    },
+    removeBarrett: function(e){
+      var target = e.target,
+        list = target.classList;
 
-      $app.append($b);
+      if(list.contains('barrett')){
+        target.remove();
+      }
     },
     getRandom: function(min, max){
       return Math.round( Math.random() * (max - min + 1) ) + min;
@@ -98,26 +98,60 @@ new Vue({
 
       return [R, G, B];
     },
-    slide: function(key) {
-      this.planePos = this.$plane.position().left;
-
-      if([37, 72].includes(key)) {
-        this.planePos += -this.speed;
-      } else if([39, 76].includes(key)) {
-        this.planePos += this.speed;
-      }
-
-      if(this.planePos < 0) {
-        this.planePos = 0;
-        return;
-      }
-
-      const {innerWidth} = window,
-            maxPos = innerWidth - this.planeWidth;
-
-      if(this.planePos > maxPos){
-        this.planePos = maxPos;
-      }
-    }
   }
+});
+
+Vue.component('shooting-object',{
+  template: '<div id="target" class="target"></div>',
+  mounted: function(){
+    this.$parent = this.$el.parentNode;
+    this.targetWidth = this.$el.clientWidth;
+    this.targetAdjustWidth = this.targetWidth / 2;
+    this.targetDefaultPos = `calc(50% - ${this.targetAdjustWidth}px)`
+    this.bindEvent();
+  },
+  methods: {
+    bindEvent: function(){
+      this.$el.addEventListener('transitionend', () => {
+        this.resetTargetPos();
+      });
+
+      this.$parent.addEventListener('shoot', () => {
+        this.avoidTarget();
+      }, false);
+
+    },
+    avoidTarget: function() {
+      const random = this.getRandom(0, 2);
+
+      switch (random) {
+        case 0:
+        this.slideTarget('-100vw');
+        break;
+        case 1:
+        this.slideTarget('100vw');
+        break;
+        case 2:
+        clearTimeout(this.timer);
+        this.$el.style.opacity = '0';
+        this.timer = setTimeout(() => {
+          this.$el.style.opacity = '1';
+        }, 300);
+        break;
+      }
+    },
+    slideTarget: function(val){
+      this.$el.style.left = val;
+    },
+    getRandom: function(min, max){
+      return Math.round( Math.random() * (max - min + 1) ) + min;
+    },
+    resetTargetPos: function(){
+      this.$el.style.left = this.targetDefaultPos;
+    },
+  }
+});
+
+new Vue({
+  el: '#app'
 });
